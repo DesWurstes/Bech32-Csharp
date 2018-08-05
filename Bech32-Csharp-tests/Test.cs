bb8759ed526e9a60f2457cd1ebe1ba08f75ada06
DesWurstes <DesWurstes@users.noreply.github.com>
2018-08-05 19:23:16 +0300

    Add files

---
+++ Bech32-Csharp-tests/Test.cs
@@ -0,0 +1,20 @@
+// TODO TODO TODO TODO WIP
+
+using System;
+using Xunit;
+using static Bech32-Csharp.Converter;
+
+namespace Bech32-Csharp-tests
+{
+	public static class Test
+	{
+		[Fact]
+		public static void conversionToCashAddr()
+		{
+		}
+		[Fact]
+		public static void conversionToOldAddr()
+		{
+		}
+	}
+}
